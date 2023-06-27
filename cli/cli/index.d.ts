5d38ad83a80e5f8604e61abcf9f6229752ed3693
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-06-27 15:18:58 +0200

    remove compile output

--- cli/cli/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { GenericApiClient } from "./api-base";
-declare class Api extends GenericApiClient {
-    baseApi: Api;
-    ping: (parameters: {}) => Promise<{
-        message: string;
-    }>;
-}
-export { Api };
-export default Api;
