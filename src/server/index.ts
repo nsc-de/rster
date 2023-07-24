027819919bbb5389a82e0171add98d79d6e85f69
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-07-24 19:09:00 +0200

    Add typedoc

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "./common.js";
 import { Context, ContextHandler } from "./context.js";
-import { Request as ExpressRequest, Response as ExpressResponse } from 'express';
+import {
+  Request as ExpressRequest,
+  Response as ExpressResponse,
+} from "express";
 import { ExpressMixin, ExpressOptions } from "./express.js";
 import { HttpError } from "./error.js";
 import { ContextCondition } from "./condition.js";
@@ -13,6 +16,7 @@ export * from "./debug.js";
 export * from "./error.js";
 export * from "./express.js";
 export * from "./info.js";
+export * as builder from "./builder.js";
 
 export class RestfulApi extends Context {
   private _options: RestfulApiOptions;
@@ -23,42 +27,52 @@ export class RestfulApi extends Context {
   }
 
   constructor(options?: RestfulApiOptionsInit) {
-
     // @ts-ignore
     super(undefined);
 
     const opts: RestfulApiOptions = {
       debug: options?.debug ?? false,
-    }
+    };
 
     this._options = opts;
 
     this._api = this;
-
   }
 
-  async handle(req: Request, res: Response, { send404 = true }: { send404?: boolean } = {}): Promise<void> {
+  async handle(
+    req: Request,
+    res: Response,
+    { send404 = true }: { send404?: boolean } = {}
+  ): Promise<void> {
     send404 = send404 ?? true;
 
     try {
       const found = await this.execute(req, res);
       if (!found && send404) {
-        await res.status(404).json({ message: `Not Found`, path: req.fullPath, api_path: req.fullApiPath, method: req.method });
+        await res
+          .status(404)
+          .json({
+            message: `Not Found`,
+            path: req.fullPath,
+            api_path: req.fullApiPath,
+            method: req.method,
+          });
       }
       res.end();
       return;
-    }
-    catch (err) {
+    } catch (err) {
       // @ts-ignore
       if (err.stack) console.error(err.stack);
       else console.error(err);
       try {
         await res.status(500).json({ message: "Internal Server Error" });
-      } catch (err) { }
+      } catch (err) {}
     }
   }
 
-  express(options?: ExpressOptions): ((req: ExpressRequest, res: ExpressResponse, next: any) => void) {
+  express(
+    options?: ExpressOptions
+  ): (req: ExpressRequest, res: ExpressResponse, next: any) => void {
     return ExpressMixin(this, options);
   }
 }
