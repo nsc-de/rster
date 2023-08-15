b7591be6a92ce7da13d537ed3f9a52ce0e5e04a8
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-08-15 12:29:14 +0200

    Create data processing lib

---
+++ src/server/database/data_processing.test.ts
@@ -0,0 +1,16 @@
+import { DataProcessingLayer } from "./data_processing";
+const layer0 = new DataProcessingLayer(undefined, {
+  test: {
+    test2(number: number) {
+      return 1 + number;
+    },
+  },
+});
+
+const layer1 = new DataProcessingLayer(layer0.functions, {
+  test: {
+    test2() {
+      this.nextLayer.test.
+    },
+  },
+});
---
+++ src/server/database/data_processing.ts
@@ -0,0 +1,127 @@
+export const PassThrough = "$__passThrough__";
+export type PassThroughType = typeof PassThrough;
+
+export type DeepMap<T, U> = {
+  [P in keyof T]: T[P] extends object ? DeepMap<T[P], U> : U;
+};
+
+export type DeepMapOptional<T, U> = {
+  [P in keyof T]?: T[P] extends object ? DeepMap<T[P], U> : U;
+};
+
+export type DataProcessingThis<
+  NEXT_LAYER extends DataProcessingBaseSchema<any> | undefined | unknown
+> = {
+  nextLayer: NEXT_LAYER;
+};
+
+export type DataProcessingBaseSchema<
+  NEXT_LAYER extends DataProcessingBaseSchema<any> | undefined | unknown
+> = {
+  [key: string]:
+    | DataProcessingBaseSchema<NEXT_LAYER>
+    | ((this: DataProcessingThis<NEXT_LAYER>, ...data: any) => any);
+};
+
+export type DataProcessingSchema<
+  NEXT_LAYER extends DataProcessingBaseSchema<any> | undefined | unknown,
+  NEXT_SCHEMA extends DataProcessingBaseSchema<NEXT_LAYER> = DataProcessingBaseSchema<NEXT_LAYER>
+> = DataProcessingBaseSchema<NEXT_LAYER> &
+  DeepMapOptional<
+    NEXT_SCHEMA,
+    PassThroughType | DataProcessingBaseSchema<NEXT_LAYER>
+  >;
+
+export type DeepMapDataProcessingSchema<
+  T,
+  EQUIVALENT,
+  NEXT_LAYER extends DataProcessingBaseSchema<any> | undefined | unknown
+> = {
+  [P in keyof T]: T[P] extends PassThroughType
+    ? P extends keyof EQUIVALENT
+      ? EQUIVALENT[P]
+      : never
+    : // eslint-disable-next-line @typescript-eslint/ban-types
+    T[P] extends Function
+    ? T
+    : T[P] extends object
+    ? DeepMapDataProcessingSchema<
+        T[P],
+        P extends keyof EQUIVALENT ? EQUIVALENT[P] : undefined,
+        NEXT_LAYER
+      >
+    : undefined;
+} & DataProcessingBaseSchema<NEXT_LAYER>;
+
+export class DataProcessingLayer<
+  INPUT_SCHEMA extends DataProcessingSchema<NEXT_LAYER>,
+  NEXT_LAYER extends DataProcessingBaseSchema<any> | undefined | unknown
+> {
+  constructor(
+    public readonly nextLayer: NEXT_LAYER,
+    public readonly inputSchema: INPUT_SCHEMA
+  ) {}
+
+  public functions: DeepMapDataProcessingSchema<
+    INPUT_SCHEMA,
+    NEXT_LAYER,
+    NEXT_LAYER
+  > = this._createFunctionMap(this.inputSchema, this.nextLayer, []);
+
+  private _createFunctionMap(
+    inputSchema: Record<string, any>,
+    outputSchema: DataProcessingBaseSchema<any> | undefined | unknown,
+    path: string[]
+  ): any {
+    const functions: Record<string, any> = {};
+    const functionThis = { nextLayer: this.nextLayer };
+
+    for (const [key, value] of Object.entries(inputSchema)) {
+      if (value === PassThrough) {
+        if (typeof outputSchema !== "object" || outputSchema === null) {
+          throw new Error(
+            `Invalid schema, cannot passthrough if output layer is not existent`
+          );
+        }
+        if (outputSchema === undefined || !(key in outputSchema)) {
+          throw new Error(
+            `Invalid schema, cannot passthrough key ${[...path, key].join(
+              "."
+            )} as it does not exist in output layer`
+          );
+        }
+
+        functions[key] = (outputSchema as any)[key];
+        continue;
+      }
+      if (typeof value === "function") {
+        // eslint-disable-next-line @typescript-eslint/ban-types
+        ((func: Function) => {
+          functions[key] = (...data: any) => {
+            return func.apply(functionThis, data);
+          };
+        })(value);
+        continue;
+      }
+      if (typeof value === "object") {
+        functions[key] = this._createFunctionMap(
+          value,
+          typeof outputSchema === "object" &&
+            outputSchema !== null &&
+            key in outputSchema
+            ? (outputSchema as any)[key]
+            : undefined,
+          [...path, key]
+        );
+        continue;
+      }
+      throw new Error(
+        `Invalid schema, key ${[...path, key].join(
+          "."
+        )} is not a function or object`
+      );
+    }
+
+    return functions;
+  }
+}
