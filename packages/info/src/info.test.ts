f4447f1e8ede5e5ddc8e367868c46de29481032f
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-09-03 01:08:00 +0200

    Migrate info tests to typescript

--- packages/info/src/info.test.ts
+++ packages/info/src/info.test.ts
@@ -1,17 +1,21 @@
 import { createSyntheticContext } from "@rster/common";
 import rest, { Context } from "@rster/basic";
 import "./info";
+
+// @ts-ignore
+const createEmptyContext = () => new Context();
+
 describe("Context", () => {
   describe("description", () => {
     it("Describe without should return the description", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         expect(this.description()).toEqual([]);
       });
     });
 
     it("Description with arguments should add to the description", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         this.description("test");
         expect(this.description()).toEqual(["test"]);
@@ -19,7 +23,7 @@ describe("Context", () => {
     });
 
     it("Test description with multiple arguments", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         this.description("test", "test2");
         expect(this.description()).toEqual(["test", "test2"]);
@@ -27,7 +31,7 @@ describe("Context", () => {
     });
 
     it("Test multiple description calls", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         this.description("test");
         this.description("test2", "test3");
@@ -36,30 +40,31 @@ describe("Context", () => {
     });
 
     it("Test getting description for another context with no description set", () => {
-      const context = new Context();
-      const context2 = new Context();
+      const context = createEmptyContext();
+      const context2 = createEmptyContext();
       context2.init(function () {
         expect(this.description(context)).toEqual([]);
       });
     });
 
     it("Test getting description for another context", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         this.description("test");
         expect(this.description(context)).toEqual(["test"]);
       });
 
-      const context2 = new Context();
+      const context2 = createEmptyContext();
       context2.init(function () {
         expect(this.description(context)).toEqual(["test"]);
       });
     });
 
     it("Test getting description for another context with more arguments to throw error", () => {
-      const context = new Context();
-      const context2 = new Context();
+      const context = createEmptyContext();
+      const context2 = createEmptyContext();
       context2.init(function () {
+        // @ts-ignore
         expect(() => this.description(context, "test")).toThrow(
           "Cannot add description to another context"
         );
@@ -69,14 +74,14 @@ describe("Context", () => {
 
   describe("field", () => {
     it("Test getting not-set field", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         expect(this.field("test")).toBeUndefined();
       });
     });
 
     it("Test setting and getting field", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         this.field("test", "hello world");
         expect(this.field("test")).toEqual("hello world");
@@ -84,30 +89,30 @@ describe("Context", () => {
     });
 
     it("Test getting unset field from another context", () => {
-      const context = new Context();
-      const context2 = new Context();
+      const context = createEmptyContext();
+      const context2 = createEmptyContext();
       context2.init(function () {
         expect(this.field(context, "test")).toBeUndefined();
       });
     });
 
     it("Test setting and getting field from another context", () => {
-      const context = new Context();
+      const context = createEmptyContext();
 
       context.init(function () {
         this.field("test", "hello world");
         expect(this.field(context, "test")).toEqual("hello world");
       });
 
-      const context2 = new Context();
+      const context2 = createEmptyContext();
       context2.init(function () {
         expect(this.field(context, "test")).toEqual("hello world");
       });
     });
 
     it("Test setting and getting field from another context", () => {
-      const context = new Context();
-      const context2 = new Context();
+      const context = createEmptyContext();
+      const context2 = createEmptyContext();
       context2.init(function () {
         this.field(context, "test", "hello world");
         expect(this.field(context, "test")).toEqual("hello world");
@@ -115,9 +120,10 @@ describe("Context", () => {
     });
 
     it("Test with invalid arguments", () => {
-      const context = new Context();
-      const context2 = new Context();
+      const context = createEmptyContext();
+      const context2 = createEmptyContext();
       context2.init(function () {
+        // @ts-ignore
         expect(() => this.field(context, 10)).toThrow("Invalid arguments");
       });
     });
@@ -125,14 +131,14 @@ describe("Context", () => {
 
   describe("fields", () => {
     it("Test getting fields", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         expect(this.fields()).toEqual({});
       });
     });
 
     it("Test setting and getting fields", () => {
-      const context = new Context();
+      const context = createEmptyContext();
       context.init(function () {
         this.field("test", "hello world");
         this.field("test2", "hello world2");
@@ -144,16 +150,16 @@ describe("Context", () => {
     });
 
     it("Test getting fields from another context (with no fields)", () => {
-      const context = new Context();
-      const context2 = new Context();
+      const context = createEmptyContext();
+      const context2 = createEmptyContext();
       context2.init(function () {
         expect(this.fields(context)).toEqual({});
       });
     });
 
     it("Test getting fields from another context", () => {
-      const context = new Context();
-      const context2 = new Context();
+      const context = createEmptyContext();
+      const context2 = createEmptyContext();
       context2.init(function () {
         this.field(context, "test", "hello world");
         this.field(context, "test2", "hello world2");
