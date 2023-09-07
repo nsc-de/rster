9b7167f61f3cdb6a994b021bf104c689734ad39d
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-09-07 13:02:17 +0200

    Update rster package index

--- packages/rster/src/index.ts
+++ packages/rster/src/index.ts
@@ -1,8 +1,8 @@
 export * as basic from "@rster/basic";
 export * as common from "@rster/common";
-export * as info from "@rster/info";
-export * as ping from "@rster/ping";
 export * as types from "@rster/types";
+import "@rster/info";
+import "@rster/ping";
 
 import rster from "@rster/basic";
 export default rster;
