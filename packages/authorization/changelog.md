082777e4f9f9e944fc12874b18ed4cec5f792524
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-09-08 10:02:54 +0200

    Add documentation to authorization.ts

--- packages/authorization/changelog.md
+++ packages/authorization/changelog.md
@@ -1,5 +1,10 @@
 # Changelog for @rster/authorization
 
+## v0.1.1
+
+- JsonWebTokenManager#sign and JsonWebTokenManager#unpack are now publicly available
+- Add documentation to full public API
+
 ## v0.1.0
 
 - Initial release
--- packages/authorization/src/authorization.ts
+++ packages/authorization/src/authorization.ts
@@ -4,7 +4,17 @@ import { $409, Context } from "@rster/basic";
 import "@rster/info";
 
 // Basic authentification verification function
-
+/**
+ * The function that will be called to verify the authentification token.
+ *
+ * @param req The request.
+ * @param res The response.
+ * @param ctx The context.
+ *
+ * @returns The token's data, false if the token is invalid or a promise that resolves to the token's data or false.
+ *
+ * @since 0.1.0
+ */
 type AuthentificationVerificationFunction<T = unknown> = (
   req: Request,
   res: Response,
@@ -18,6 +28,8 @@ declare module "@rster/basic" {
      * Will not check permissions, just that a valid authentification token is present.
      *
      * @param verificationFunction The function that will be called to verify the authentification token.
+     *
+     * @since 0.1.0
      */
     useAuthentification(
       verificationFunction: AuthentificationVerificationFunction
@@ -30,11 +42,15 @@ declare module "@rster/basic" {
      * Has no effect if useAuthentification() is not called with a verification function that supports permissions.
      *
      * @param permission The permission(s) to add to the list of required permissions.
+     *
+     * @since 0.1.0
      */
     permissions(permission: Permission | Permission[]): void;
 
     /**
      * Returns the list of required permissions for the user to access this context.
+     *
+     * @since 0.1.0
      */
     permissions(): Permission[];
   }
@@ -43,6 +59,8 @@ declare module "@rster/basic" {
 declare module "@rster/common" {
   /**
    * The authentification token's data.
+   *
+   * @since 0.1.0
    */
   interface Request {
     token_data: unknown;
@@ -78,11 +96,42 @@ Context.prototype.permissions = function (
   this.field("required_permissions", required_permissions);
 };
 
+/**
+ * The function that will be called to verify the json web token.
+ *
+ * @typeParam T The type of the token's data.
+ * @param token The token to verify.
+ * @returns The data the tokn should contain.
+ * @throws Throw an error if the token is invalid.
+ *
+ * @since 0.1.0
+ */
 export type JsonWebTokenTester<T> = (token: JsonWebTokenData) => T;
 
+/**
+ * The data contained in the json web token.
+ *
+ * @since 0.1.0
+ */
 export type JsonWebTokenData = Record<string, unknown>;
 
+/**
+ * A permission that can be granted to a user or needed to access a context.
+ *
+ * @since 0.1.0
+ */
 export type Permission = string;
+
+/**
+ * Function to verify a permission. Returns true if the permission is granted, false otherwise.
+ *
+ * @param permissions The permissions the user has.
+ * @param permission The permission to check.
+ *
+ * @returns True if the permission is granted, false otherwise.
+ *
+ * @since 0.1.0
+ */
 export function permissionIncluded(
   permissions: Permission[] | Permission,
   permission: Permission
@@ -106,19 +155,57 @@ export function permissionIncluded(
   }
 }
 
+/**
+ * Properties of a JWT
+ *
+ * @typeParam TEST_ARGS The type of the arguments needed to test the token.
+ * @typeParam CREATION_ARGS The type of the arguments needed to create the token.
+ * @typeParam JWT_PROPS The type of the token's data.
+ *
+ * @since 0.1.0
+ */
 export abstract class JsonWebTokenProperty<
   TEST_ARGS,
   CREATION_ARGS,
   JWT_PROPS extends JsonWebTokenData
 > {
   constructor() {}
+
+  /**
+   * Checks if the token is valid.
+   *
+   * @param jwt The JWT to check.
+   * @param args The arguments needed to check the token.
+   * @returns True if the token is valid, false otherwise.
+   *
+   * @since 0.1.0
+   */
   public abstract check(jwt: JWT_PROPS, args: TEST_ARGS): boolean;
+
+  /**
+   * Transforms the token.
+   *
+   * @param jwt The JWT to transform.
+   * @param args The arguments needed to transform the token.
+   * @returns The transformed token.
+   *
+   * @since 0.1.0
+   */
   public abstract transform(
     jwt: JsonWebTokenData,
     args: CREATION_ARGS
   ): JWT_PROPS;
 }
 
+/**
+ * Property map for a JWT containing Expiration date.
+ *
+ * @typeParam TEST_ARGS The type of the arguments needed to test the token.
+ * @typeParam CREATION_ARGS The type of the arguments needed to create the token.
+ * @typeParam JWT_PROPS The type of the token's data.
+ *
+ * @since 0.1.0
+ */
 export class JsonWebTokenPropertyExpireAt extends JsonWebTokenProperty<
   Record<string, never>,
   { duration: number } | { expire_at: number },
@@ -127,10 +214,26 @@ export class JsonWebTokenPropertyExpireAt extends JsonWebTokenProperty<
   constructor() {
     super();
   }
+
+  /**
+   * Checks if the token is valid.
+   * @param jwt The JWT to check.
+   * @returns true if the token is valid, false otherwise.
+   *
+   * @since 0.1.0
+   */
   public check(jwt: { exp: number }): boolean {
     return jwt.exp > Date.now();
   }
 
+  /**
+   * Transforms the JWT by adding an expiration date.
+   * @param jwt The JWT to transform.
+   * @param args The arguments needed to transform the JWT.
+   * @returns The transformed JWT.
+   *
+   * @since 0.1.0
+   */
   public transform(
     jwt: JsonWebTokenData,
     args: { duration: number } | { expire_at: number }
@@ -143,6 +246,15 @@ export class JsonWebTokenPropertyExpireAt extends JsonWebTokenProperty<
   }
 }
 
+/**
+ * Property map for a JWT containing permissions.
+ *
+ * @typeParam TEST_ARGS The type of the arguments needed to test the token.
+ * @typeParam CREATION_ARGS The type of the arguments needed to create the token.
+ * @typeParam JWT_PROPS The type of the token's data.
+ *
+ * @since 0.1.0
+ */
 export class JsonWebTokenPropertyPermissions extends JsonWebTokenProperty<
   { permissions_required: Permission[] },
   { permissions: Permission[] },
@@ -151,29 +263,79 @@ export class JsonWebTokenPropertyPermissions extends JsonWebTokenProperty<
   constructor() {
     super();
   }
+
+  /**
+   * Tests if the token contains the required permissions.
+   * @param jwt The JWT to test.
+   * @param param1 The arguments needed to test the token.
+   * @returns True if the token contains the required permissions, false otherwise.
+   *
+   * @since 0.1.0
+   */
   public check(
-    jwt: { perms: Permission[] },
-    { permissions_required }: { permissions_required: Permission[] }
+    jwt: {
+      /**
+       * The permissions the user has.
+       *
+       * @since 0.1.0
+       */
+      perms: Permission[];
+    },
+    {
+      permissions_required,
+    }: {
+      /**
+       * The permissions required to access the context.
+       *
+       * @since 0.1.0
+       */
+      permissions_required: Permission[];
+    }
   ) {
     return permissions_required.every((permission) =>
       permissionIncluded(jwt.perms, permission)
     );
   }
 
+  /**
+   * Transforms the JWT by adding the permissions.
+   * @param jwt The JWT to transform.
+   * @param args The arguments needed to transform the JWT.
+   * @returns The transformed JWT.
+   *
+   * @since 0.1.0
+   */
   public transform(
     jwt: JsonWebTokenData,
-    args: { permissions: Permission[] }
+    args: {
+      /**
+       * The permissions to add to the JWT.
+       *
+       * @since 0.1.0
+       */
+      permissions: Permission[];
+    }
   ): { perms: Permission[] } {
     return { ...jwt, perms: args.permissions };
   }
 }
 
+/**
+ * Gets the type of the token's data. Used internally to create types for the token's data.
+ *
+ * @since 0.1.0
+ */
 type TokenTypeForProperty<
   T extends JsonWebTokenProperty<unknown, unknown, JsonWebTokenData>
 > = T extends JsonWebTokenProperty<unknown, unknown, infer JWT_PROPS>
   ? JWT_PROPS
   : never;
 
+/**
+ * Transforms an array of TokenTypes to a union of their properties. Used internally to create types for the token's data.
+ *
+ * @since 0.1.0
+ */
 type TokenTypeMatchingForAllProperties<
   PROPS extends JsonWebTokenProperty<unknown, unknown, JsonWebTokenData>[]
 > = PROPS extends Array<infer T>
@@ -182,6 +344,12 @@ type TokenTypeMatchingForAllProperties<
     : never
   : never;
 
+/**
+ * Get the creation args for all given properties. Used internally to create types for the token's data.
+ *
+ * @since 0.1.0
+ *
+ */
 type CREATION_ARGS_FOR_ALL_PROPERTIES<
   PROPS extends JsonWebTokenProperty<unknown, unknown, JsonWebTokenData>[]
 > = PROPS extends Array<infer T>
@@ -194,6 +362,11 @@ type CREATION_ARGS_FOR_ALL_PROPERTIES<
     : never
   : never;
 
+/**
+ * Get the test args for all given properties. Used internally to create types for the token's data.
+ *
+ * @since 0.1.0
+ */
 type TEST_ARGS_FOR_ALL_PROPERTIES<
   PROPS extends JsonWebTokenProperty<unknown, unknown, JsonWebTokenData>[]
 > = PROPS extends Array<infer T>
@@ -202,6 +375,16 @@ type TEST_ARGS_FOR_ALL_PROPERTIES<
     : never
   : never;
 
+/**
+ * Manages the creation and verification of json web tokens.
+ *
+ * @typeParam PROPS The properties of the token.
+ * @typeParam JWT_PROPS The type of the token's data.
+ * @typeParam CREATION_ARGS The type of the arguments needed to create the token.
+ * @typeParam TEST_ARGS The type of the arguments needed to test the token.
+ *
+ * @since 0.1.0
+ */
 export class JsonWebTokenManager<
   PROPS extends JsonWebTokenProperty<unknown, unknown, JsonWebTokenData>[],
   JWT_PROPS extends JsonWebTokenData &
@@ -209,11 +392,25 @@ export class JsonWebTokenManager<
   CREATION_ARGS extends CREATION_ARGS_FOR_ALL_PROPERTIES<PROPS> = CREATION_ARGS_FOR_ALL_PROPERTIES<PROPS>,
   TEST_ARGS extends TEST_ARGS_FOR_ALL_PROPERTIES<PROPS> = TEST_ARGS_FOR_ALL_PROPERTIES<PROPS>
 > {
+  /**
+   * Creates a new JsonWebTokenManager.
+   * @param secret The secret used to sign the token. Must be the same for all instances of the same type and **must** be kept secret.
+   * @param properties The properties of the token.
+   *
+   * @since 0.1.0
+   */
   constructor(
     private readonly secret: string,
     private readonly properties: PROPS = [] as unknown as PROPS
   ) {}
 
+  /**
+   * Creates a new token.
+   * @param args The arguments needed to create the token.
+   * @returns The created and signed token.
+   *
+   * @since 0.1.0
+   */
   public create(args: CREATION_ARGS): string {
     let jwt: JsonWebTokenData = {};
 
@@ -224,6 +421,15 @@ export class JsonWebTokenManager<
     return this.sign(jwt as JWT_PROPS);
   }
 
+  /**
+   * Verifies a token.
+   * @param token The token to verify.
+   * @param args The arguments needed to verify the token.
+   * @returns The token's data.
+   * @throws Throws an error if the token is invalid.
+   *
+   * @since 0.1.0
+   */
   public verify(token: string, args: TEST_ARGS): JWT_PROPS {
     const jwt = this.unpack(token) as JWT_PROPS;
 
@@ -234,6 +440,13 @@ export class JsonWebTokenManager<
     return jwt;
   }
 
+  /**
+   * Signs the token.
+   * @param payload The payload to sign.
+   * @returns The signed token.
+   *
+   * @since 0.1.1
+   */
   public sign(payload: JWT_PROPS): string {
     const payload_str = JSON.stringify(payload);
     const signature = crypto
@@ -246,7 +459,15 @@ export class JsonWebTokenManager<
     return `${payload_b64}:${signature}`;
   }
 
-  private unpack(token: string): JWT_PROPS {
+  /**
+   * Unpacks the token.
+   * @param token The token to unpack.
+   * @returns The token's data.
+   * @throws Throws an error if the token is invalid.
+   *
+   * @since 0.1.1
+   */
+  public unpack(token: string): JWT_PROPS {
     const splitted = token.split(":");
     if (splitted.length !== 2) throw $409("Invalid token");
 
