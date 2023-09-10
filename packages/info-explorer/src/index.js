d5dd3922305ac03b66752d12ee08a53630db10d4
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-09-10 11:30:30 +0200

    Fixed info-explorer package build

--- packages/info-explorer/src/index.js
+++ packages/info-explorer/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.sass";
-import App from "./app.js";
+import App from "./app";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
