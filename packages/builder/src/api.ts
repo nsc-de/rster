ac8c3a05cb9b7662dc4dc1c7832cf305d8d387af
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-09-14 18:33:37 +0200

    Write documentation for api.ts in builder

--- packages/builder/src/api.ts
+++ packages/builder/src/api.ts
@@ -38,6 +38,14 @@ export interface RsterApiJson {
   methods: RsterApiMethodJson[];
 }
 
+/**
+ * Rster api class. Used to create an api with `@rster/builder`.
+ *
+ * _We use type parameters to more specifically index the typing of the api. This allows us to get the correct typing for the `native` method. From a functional perspective, the type parameters are not needed._
+ * @typeParam NAME - The name of the api.
+ * @typeParam MODULES - The modules of the api.
+ * @typeParam METHODS - The methods of the api.
+ */
 export class RsterApi<
   NAME extends string,
   MODULES extends { [key: string]: RsterApiModule<typeof key, any, any> },
@@ -45,19 +53,58 @@ export class RsterApi<
     [key: string]: RsterApiMethod<typeof key, AnyParameterDeclaration>;
   }
 > {
+  /**
+   * The constructor of the {@link RsterApi} class.
+   *
+   * @param name The name of the api.
+   * @param description The description of the api.
+   * @param modules The modules of the api.
+   * @param methods The methods of the api.
+   */
+
   constructor(
+    /**
+     * The name of the api.
+     */
     public readonly name: NAME,
+
+    /**
+     * The description of the api.
+     */
     public readonly description: string[],
+
+    /**
+     * The modules of the api.
+     * @see {@link RsterApi.moduleList}
+     */
     public readonly modules: MODULES,
+
+    /**
+     * The methods of the api.
+     * @see {@link RsterApi.methodList}
+     */
     public readonly methods: METHODS
   ) {
     this.moduleList = Object.values(modules) as Values<MODULES>;
     this.methodList = Object.values(methods) as Values<METHODS>;
   }
 
+  /**
+   * A list of the modules of the api.
+   * @see {@link RsterApi.modules}
+   */
   public readonly moduleList: Values<MODULES>;
+
+  /**
+   * A list of the methods of the api.
+   * @see {@link RsterApi.methods}
+   */
   public readonly methodList: Values<METHODS>;
 
+  /**
+   * Returns the json representation of the api.
+   * @see RsterApiJson
+   */
   public json(): RsterApiJson {
     return {
       name: this.name,
@@ -67,6 +114,10 @@ export class RsterApi<
     };
   }
 
+  /**
+   * Returns the api as a restful api. {@link RestfulApi}
+   * @see {@link RestfulApi}
+   */
   public rest(): RestfulApi {
     return rest(() => {
       Context.current.description(...this.description);
@@ -81,6 +132,9 @@ export class RsterApi<
     });
   }
 
+  /**
+   * Returns the api as a native object. This is userful if you want to use the api definition as functions on your server-side, so you don't have to define the functionality in another place
+   */
   public native(): RemoveNeverProperties<{
     [key in keyof MODULES]: key extends keyof METHODS
       ? ReturnType<MODULES[key]["native"]> & ReturnType<METHODS[key]["native"]>
@@ -117,6 +171,15 @@ export class RsterApi<
   }
 }
 
+/**
+ * Shortcut function to create a {@link RsterApi} object.
+ *
+ * @param name The name of the api.
+ * @param description The description of the api.
+ * @param modules The modules of the api.
+ * @param methods The methods of the api.
+ * @returns A new {@link RsterApi} object.
+ */
 export function api<
   NAME extends string,
   MODULES extends { [key: string]: RsterApiModule<typeof key, any, any> },
