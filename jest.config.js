01fcf01b6ee7bb1e31852a49f3030974ee8ee76c
Nicolas Schmidt <64435955+nsc-de@users.noreply.github.com>
2023-10-11 16:59:43 +0200

    Fix jest configuration

--- jest.config.js
+++ jest.config.js
@@ -131,7 +131,7 @@ const config = {
   rootDir: __dirname,
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: pkgs,
+  roots: pkgs.map((p) => path.join(p, "src")),
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
